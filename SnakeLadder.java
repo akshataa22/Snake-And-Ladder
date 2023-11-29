6e842769cf3a3c3f8c7a7d7a27c5840ee205a865
akshataa22 <dhanawadeakshata53@gmail.com>
2023-11-30 01:50:16 +0530

    Welcome Message

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package Day9;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder game Program.");
+
+        int player = 0;            //initializing the player position
+    }
+}
