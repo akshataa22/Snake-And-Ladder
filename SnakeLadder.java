623defff02f4a9b41349d04a742b427fb8fcbab4
akshataa22 <dhanawadeakshata53@gmail.com>
2023-11-30 01:54:22 +0530

    initializing the player position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,6 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder game Program.");
 
-        int player = 0;            //initializing the player position
+        int player = 0;         //initializing the player position
     }
 }
